aadb79ee83a01f92019d7c62886834fd3633d5ec
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-11-24 12:12:16 +0530

    ok

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 
-const Admin = () => {
+const Admin = (props) => {
     return (
         <div>
             <div className='h-full flex justify-center'>
-                <h1>Table</h1>
+                <table>
+                    <tr>
+                        <th>props</th>
+                        <th>Contact</th>
+                        <th>Country</th>
+                    </tr>
+                    <tr>
+                        <td>Alfreds Futterkiste</td>
+                        <td>Maria Anders</td>
+                        <td>Germany</td>
+                    </tr>
+                    <tr>
+                        <td>Centro comercial Moctezuma</td>
+                        <td>Francisco Chang</td>
+                        <td>Mexico</td>
+                    </tr>
+                </table>
             </div>
         </div>
     )
